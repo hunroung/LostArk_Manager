d6c9553144602d3d84bd3f47b9d58a6df5d59f07
junhee <hunroung02@naver.com>
2022-11-25 19:10:59 +0900

    + 구현 파일, 헤더 파일 추가

---
+++ 0_basic_information/RockHeader.h
@@ -0,0 +1,36 @@
+#include <ctime>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace System;
+
+namespace Rock {
+	public ref class Rocksimulator 
+	{
+	public:
+		int abil1_count = 0;
+		int abil2_count = 0;
+		int abil3_count = 0;
+		int abil1_success_count = 0;
+		int abil2_success_count = 0;
+		int abil3_success_count = 0;
+		int p = 75;
+		Rocksimulator() {
+			srand((unsigned int)time(NULL));
+		}
+		int rock_simul() {
+			int num = rand() % 100;
+			if (num < p) {
+				if (p != 25)
+					p -= 10;
+				return 1;
+			}
+			else {
+				if (p != 75)
+					p += 10;
+				return 2;
+			}
+		}
+	};
+}
---
+++ 0_basic_information/SimulForm.cpp
@@ -0,0 +1,2 @@
+#include "SimulForm.h"
+
